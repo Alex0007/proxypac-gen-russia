0963e08c7ac1d7b711559d3a5ff0c563eac80976
Alex0007 <alex0007lolpvp@gmail.com>
2014-07-05 03:21:51 +0600

    update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var express = require('express');
 var schedule = require('node-schedule');
 var request = require('request');
+var async = require('async');
 
 var dump_url = 'https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv';
 
@@ -13,12 +14,38 @@ app.use(express.static(__dirname + '/static'));
 //     res.send('Hello World');
 // });
 
-function generate_pac() {
-    console.log('generating new proxy pac');
-    request('https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv')
-    .pipe(fs.createWriteStream('dump.txt'), function(){console.log("success")});
+function find_IP(filename) {
+    //parse ip adresses from file
+    console.log("file saved succesfully");
+    console.log("finding ips");
+    var fs = require('fs')
+    fs.readFile(filename, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var re = /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]);/g;
+        var ips = data.match(re);
+        for (var key in ips) {
+            ips[key] = ips[key].slice(0, -1);
+        }
+        console.log(ips);
+    });
 }
 
+
+function generate_pac() {
+    console.log('generating new proxy pac');
+    //saving file with rkn dump
+    var file = fs.createWriteStream("dump.txt");
+    var r = request("https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv").pipe(file);
+
+
+    r.on("finish", function () {
+        find_IP("dump.txt");
+
+
+    });
+}
 var server = app.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port %d', server.address().port);
 });

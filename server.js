2ba6e5b96fe88b73083f4883540c4114f67cca8f
Alex0007 <Alex0007lolpvp@gmail.com>
2014-07-04 20:17:21 +0600

    update

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var schedule = require('node-schedule');
+
 var app = express();
 app.use(express.static(__dirname + '/static'));
 
@@ -6,6 +8,10 @@ app.use(express.static(__dirname + '/static'));
 // app.get('/', function(req, res) {
 //     res.send('Hello World');
 // });
+
+var j = schedule.scheduleJob('0 * * * * *', function(){
+    console.log('Time for tea!');
+});
 
 var server = app.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port %d', server.address().port);

64b72f9af48318182e180b2f52a06c456562e1d4
Alex0007 <Alex0007lolpvp@gmail.com>
2016-02-23 09:56:55 +0500

    production env

--- config.js
+++ config.js
@@ -8,7 +8,11 @@ let config = {
       PROXYSTRING: 'SOCKS5 127.0.0.1:9050',
       HOST_DOMAIN: 'proxypac-gen-russia.alex0007.ru'
     },
-    production: {}
+    production: {
+      DUMP_URL: 'https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv',
+      PROXYSTRING: 'SOCKS5 127.0.0.1:9050',
+      HOST_DOMAIN: 'proxypac-gen-russia.alex0007.ru'
+    }
   }
 }
 
